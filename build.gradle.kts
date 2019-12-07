37a0ba0850a4e5e505fe0ec69907f03e8b11b8aa
Sean C. Sullivan <github@seansullivan.com>
2019-12-07 15:46:54 -0800

    add Gradle 6.0.1

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+
+plugins {
+    `java-library`
+}
+
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
