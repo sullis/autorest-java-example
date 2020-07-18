3389bf7edd1e30d687b73c2ebf75024e6e5e0fbe
Sean C. Sullivan <github@seansullivan.com>
2020-07-17 19:14:02 -0700

    autorest client runtime 1.7.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.microsoft.rest:client-runtime:1.7.2")
+    implementation("com.microsoft.rest:client-runtime:1.7.5")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
